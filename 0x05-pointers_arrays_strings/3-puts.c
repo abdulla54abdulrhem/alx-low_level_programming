b08e9557a2d0674d2d9be1f9ec974b2392016a6b
abdo <body5401@gmail.com>
2023-03-27 07:24:41 -0700

    added 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+#include "_putchar.c"
+/**
+ * _puts - function thar prints a string
+ * @str : pointer to a string
+ *
+ */
+void _puts(char *str)
+{
+int len = _strlen(*str);
+int i;
+  
+for (i = 0; i < len; i++)
+{
+_putchar((*str));
+str++;
+}
+_putchar('\n');
+}
