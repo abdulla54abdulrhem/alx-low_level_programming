6842fe7f37278360dc051e55c9e5a1995ea0c6e2
abdo <body5401@gmail.com>
2023-03-27 07:16:32 -0700

    added 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _strlen(char *s) - length of the string
+ * @s : pointer to string
+ *
+ * Return: length of str
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while(*s != '\0')
+{
+length ++;
+*s++;
+}
+return (length);
+}
