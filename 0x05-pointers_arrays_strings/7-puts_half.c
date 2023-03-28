329fd036c8107a7ea9462cc25e91010c194aba29
abdo <body5401@gmail.com>
2023-03-27 19:47:53 -0700

    added 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,16 @@ void puts_half(char *str)
 {
 int n = _strlen(str);
 int nn = n;
+int i;
 if (n % 2 == 1)
 n = (n - 1) / 2;
 else
+{
 n = n / 2;
-int i;
+}
 for (i = 0; i < nn; i++)
 {
-_putcahr(str[i]);
+_putchar(str[i]);
 }
 _putchar('\n');
 }
