6088899714f2e5c93785d96628665f69a5ef4bd5
abdo <body5401@gmail.com>
2023-03-30 16:02:57 -0700

    added 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - to uppercase
+ * @s : string pointer
+ *
+ *
+ */
+char *string_toupper(char *s)
+{
+char *tmp
+tmp = s;
+while((*s) != '\0')
+{
+if ((*s) >= 'a' && (*s) <= 'z')
+(*s) = (*s) - 32;
+s++;
+}
+return (tmp);
+}
