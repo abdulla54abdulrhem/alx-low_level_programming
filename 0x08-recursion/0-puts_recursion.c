e13af2dbf297dc43da775bd90874e33659407676
abdo <body5401@gmail.com>
2023-04-05 06:29:50 -0700

    added 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-if((*s) != '\0')
-return;
-_putchar((*s));
-s++;
-_puts_recursion(s);
+if (*s)
+{
+_putchar(*s);
+_put_recursion(++s);      
+}
+else
+_putchar('\n');
 }
