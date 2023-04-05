984108719bb06d80b33f3ece35ed4ac6d244e508
abdo <body5401@gmail.com>
2023-04-05 07:55:53 -0700

    fix 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,28 @@
 #include "main.h"
 /**
+* help - loop recursively
+* @i : integer
+* @n : integer
+* Retrun: answer
+*/
+int help(int i,int n)
+{
+if (n % i == 0 && n != i)
+return (0);
+if (n % i != 0 && i < n)
+return (help(i+1,n));
+return (1);
+}
+/**
 * is_prime_number - if prime or not
 * @n : integer
 * Return: 1 if prime 0 if not
 */
 int is_prime_number(int n)
 {
-int ans;
 int i;
-ans = 1;
-for (i = 2; i *i <= n; i++)
-{
-if (n % i == 0)
-{
-ans = 0;
-break;
-}
-}
-return (ans);
+i = 2;
+if (n < 2)
+return (0);
+return (help(i,n));
 }
