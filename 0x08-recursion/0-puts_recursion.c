4d434b7d0a4f028da14779a945225dec43f02e1a
abdo <body5401@gmail.com>
2023-04-05 06:25:36 -0700

    added 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s : a pointer to string
+ * 
+ *
+ */
+void _puts_recursion(char *s)
+{
+if((*s) != '\0')
+return;
+_putchar((*s));
+s++;
+_puts_recursion(s);
+}
