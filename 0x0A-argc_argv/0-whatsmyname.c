fbbebe239144b31aa36ffff9b8483a000c52d7eb
abdo <body5401@gmail.com>
2023-04-11 07:55:00 -0700

    added 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,11 @@
 * main - main function
 * @argc : counter
 * @argv : argument vector
-* Return: 0
+* Return: always zero
 */
 int main(int argc, char *argv[])
 {
-(void)(argc);
+(void) argc;
 printf("%s\n", argv[0]);
 return (0);
 }
