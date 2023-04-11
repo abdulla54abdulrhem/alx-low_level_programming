b22d40ee26026f1f730e88fc77726ded96420b2c
abdo <body5401@gmail.com>
2023-04-11 08:17:07 -0700

    added 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,12 @@
  */
 int main(int argc, char *argv[])
 {
+int tmp;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-int tmp;
 tmp = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n",tmp);
 return (0);
