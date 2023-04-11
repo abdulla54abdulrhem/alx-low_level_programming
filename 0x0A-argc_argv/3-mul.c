639d2d19e4f648eef1120dd547a8674eb137bbca
abdo <body5401@gmail.com>
2023-04-11 08:18:53 -0700

    added 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,15 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
  * main - main function
  * @argc : integer
  * @argv : argumnet vector
  * Return: 0 or 1
- *
- *
- *
- *
  */
 int main(int argc, char *argv[])
 {
@@ -21,6 +16,6 @@ printf("Error\n");
 return (1);
 }
 tmp = atoi(argv[1]) * atoi(argv[2]);
-printf("%d\n",tmp);
+printf("%d\n", tmp);
 return (0);
 }
