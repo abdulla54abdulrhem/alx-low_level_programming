33fcf4a74d043fa0285294d79ec1dec53e987759
abdo <body5401@gmail.com>
2023-04-11 18:28:18 -0700

    added 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,9 @@ char *create_array(unsigned int size, char c)
 {
 int i;
 char *array;
-array = malloc(size * sizeof(char));
+array = malloc(sizeof(char) * size);
+if (array == NULL)
+return (NULL);
 for (i = 0; i < size; i++)
 {
 *array[i] = c;
