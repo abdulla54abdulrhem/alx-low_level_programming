cd13fb6790688b2abae8c55e3322ca5fc3b3c237
abdo <body5401@gmail.com>
2023-04-14 16:47:54 -0700

    fixed 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,48 +2,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * string_nconcat - concats strings
- * @s1: string one
- * @s2: string two
- * @n: n amount of bytes
- * Return: return a char val
+ * string_nconcat - Concatenates two strings, up to n bytes of s2
+ *
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The maximum number of bytes from s2 to concatenate
+ *
+ * Return: A pointer to the concatenated string or NULL if malloc fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *tmp;
-unsigned int i;
-unsigned int len1;
-unsigned int len2;
-char *ans;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-len1 = 0;
-len2 = 0;
-tmp = s1;
-while (*tmp != '\0')
-{
-len1++;
-tmp++;
-}
-tmp = s2;
-while (*tmp != '\0')
-{
-len2++;
-tmp++;
-}
-if (len2 < n)
-n = len2;
-ans = malloc(sizeof(char) * n + len1 + 1);
-if (ans == NULL)
-return (NULL);
-for (i = 0; i < len1; i++)
-{
-ans[i] = s1[i];
-}
-for (i = 0; i < n; i++)
-ans[i + len1] = s2[i];
-ans[i+len1] = '\0';
-return (ans);
+	char *tmp, *ans;
+	unsigned int i, len1 = 0, len2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	tmp = s1;
+	while (*tmp)
+	{
+		len1++;
+		tmp++;
+	}
+
+	tmp = s2;
+	while (*tmp)
+	{
+		len2++;
+		tmp++;
+	}
+
+	if (len2 < n)
+		n = len2;
+
+	ans = malloc(sizeof(char) * (len1 + n + 1));
+	if (ans == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		ans[i] = s1[i];
+
+	for (i = 0; i < n; i++)
+		ans[i + len1] = s2[i];
+
+	ans[i + len1] = '\0';
+	return (ans);
 }
