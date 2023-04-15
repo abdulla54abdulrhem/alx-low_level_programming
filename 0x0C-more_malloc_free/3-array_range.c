bf9740b7835ecfb559d65dcff155e68a5ddec378
abdo <body5401@gmail.com>
2023-04-14 18:14:12 -0700

    added 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,14 +11,16 @@
  */
 int *array_range(int min, int max)
 {
-    int *arr;
-    int i;
-    arr = malloc(sizeof(int) * (max - min + 1));
-    if (arr == NULL)
-        return (NULL);
-    for (i = min; i <= max; i++)
-    {
-        arr[i - min] = i;
-    }
-    return (arr);
+int *arr;
+int i;
+if (min > max)
+return (NULL);
+arr = malloc(sizeof(int) * (max - min + 1));
+if (arr == NULL)
+return (NULL);
+for (i = min; i <= max; i++)
+{
+arr[i - min] = i;
+}
+return (arr);
 }
