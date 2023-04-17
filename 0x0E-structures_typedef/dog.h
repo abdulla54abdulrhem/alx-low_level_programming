d8a048ebb4f707453df84142db23a49665f477b1
abdo <body5401@gmail.com>
2023-04-17 16:13:53 -0700

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - dog
+ * @name: the name of dog
+ * @age: float
+ * @owner: name of owner
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+}
+  typedef dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
