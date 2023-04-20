e50ae90896b9fd068b2749ddf7d02bb4502816d3
abdo <body5401@gmail.com>
2023-04-20 13:36:03 -0700

    fixed betty in 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 /**
  * array_iterator - Execute a callback function on each element of an array.
- *
  * @array: A pointer to the array to iterate over.
  * @size: The number of elements in the array.
  * @action: A function pointer to the callback function to execute on each element of the array.
@@ -11,12 +10,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
   unsigned int j;
-
   if (action == NULL || array == NULL)
   {
     exit(98);
   }
-
   for (j = 0; j < size; j++)
   {
     action(array[j]);
