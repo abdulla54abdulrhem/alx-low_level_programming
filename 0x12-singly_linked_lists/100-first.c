748451bde61c29b88a98fa1110dc38f43cc77dfc
abdo <body5401@gmail.com>
2023-04-27 19:37:37 -0700

    added 1--

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * first - Function executed before main.
+ *
+ * Description: This function uses the constructor attribute to specify that
+ * it should be executed before main(). It prints a message to the standard
+ * output.
+ *
+ * Return: void.
+ */
+void first(void) __attribute__((constructor));
+
+void first(void)
+{
+    printf("You're beat! and yet, you must allow,\n"
+           "I bore my house upon my back!\n");
+}
