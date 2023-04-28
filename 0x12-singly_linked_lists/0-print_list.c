bbe280e136d0fc797c9135cde152b094ca9f128e
abdo <body5401@gmail.com>
2023-04-27 18:45:38 -0700

    added 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *  print_list - print linked list
+ * @h: linked list pointer
+ * Return: counter
+ */
+size_t print_list(const list_t *h)
+{
+int i;
+i = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n",h->len,h->str);
+h = h->next;
+i++;
+}
+return (i);
+}
