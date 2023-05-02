9a3c5ec59bea29fb81ee9338d9200542f3c64974
abdo <body5401@gmail.com>
2023-05-02 10:31:03 -0700

    try to fix 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,9 @@
 #include "lists.h"
 /**
- * print_listint - Prints all the elements of a listint_t list.
- *
- * @h: A pointer to the head of the listint_t list.
- *
- * Return: int.
- */
+* print_listint - Prints all the elements of a listint_t list.
+* @h: A pointer to the head of the listint_t list.
+* Return: int.
+*/
 size_t print_listint(const listint_t *h)
 {
   int counter;
